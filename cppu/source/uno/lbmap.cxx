bd0a7495514dfa8e0c489be6f973960fc66a862f
Thorsten Behrens <thorsten.behrens@allotropia.de>
2021-09-25 18:03:31 +0200

    WASM add minimal dummy bridge

--- cppu/source/uno/lbmap.cxx
+++ cppu/source/uno/lbmap.cxx
@@ -328,8 +328,10 @@ static void setNegativeBridge( const OUString & rBridgeName )
 
 static uno_ext_getMappingFunc selectMapFunc( const OUString & rBridgeName )
 {
+#ifndef EMSCRIPTEN
     if (rBridgeName.equalsAscii( CPPU_CURRENT_LANGUAGE_BINDING_NAME "_uno" ))
         return CPPU_ENV_uno_ext_getMapping;
+#endif
 #if HAVE_FEATURE_JAVA
     if (rBridgeName.equalsAscii( "java" "_uno" ))
         return java_uno_ext_getMapping;
@@ -421,6 +423,7 @@ static Mapping loadExternalMapping(
             OSL_ASSERT( aExt.is() );
             if (aExt.is())
                 return aExt;
+            SAL_INFO("cppu", "Could not load external mapping for " << aName);
         }
 #else
         // find proper lib
